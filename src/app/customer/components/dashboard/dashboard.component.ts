709c917d507df1284af763c4511cac414ad4358d
Noby <2317629@cognizant.com>
2024-05-31 15:48:19 +0530

    Initial commit

---
+++ src/app/customer/components/dashboard/dashboard.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from '../../services/customer.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  products: any[] = [];
+  searchProductForm!: FormGroup;
+
+  constructor(private customerService: CustomerService,
+    private fb:FormBuilder,
+    private snackBar: MatSnackBar,
+  ){}
+
+  ngOnInit(){
+    this.getAllProducts();
+    this.searchProductForm=this.fb.group({
+      title: [null,[Validators.required]]
+    })
+
+  }
+
+
+  getAllProducts(){
+    this.products =[];
+    this.customerService.getAllProducts().subscribe((res) => {
+      res.forEach(element => {
+        element.processedImg ='data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+  }
+
+  submitForm(){
+    this.products =[];
+    const title=this.searchProductForm.get('title')!.value;
+    this.customerService.getAllProductsByName(title).subscribe((res) => {
+      res.forEach(element => {
+        element.processedImg ='data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+  }
+
+
+addToCart(id: any) {
+  this.customerService.addToCart(id).subscribe({
+      error: error => {
+        if (error.status === 409) {
+          this.snackBar.open("Product is already in the cart", "Close", { duration: 5000 });
+        } else if (error.status === 404) {
+          this.snackBar.open("User or product not found", "Close", { duration: 5000 });
+        } else {
+          this.snackBar.open("Product added to cart successfully", "Close", { duration: 5000 });
+        }
+      }
+});
+
+}
+
+
+}
